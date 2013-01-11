70c1fac71aa50129d4f2e614491d78e9e128f32a
d3kod <xilunas@gmail.com>
2013-01-11 00:21:17 -0800

    Refactoring: Abstracting away from D3Shapes and introducing D3Sprite objects instead. Long way to go - CatchNet, all the FloatingObjects and Prey

--- TheHunt/src/d3kod/d3gles20/D3GLES20.java
+++ TheHunt/src/d3kod/d3gles20/D3GLES20.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import android.graphics.PointF;
 import android.util.Log;
 import d3kod.d3gles20.shapes.D3Circle;
 import d3kod.d3gles20.shapes.D3FadingShape;
@@ -17,22 +18,22 @@ public class D3GLES20 {
 	static final String TAG = "D3GLES20";
 	private static final int TEMP_CIRCLE_TICKS = 50;
 	
-	private HashMap<Integer, D3Shape> shapes;
+	private HashMap<Integer, D3Sprite> sprites;
 	private HashMap<Integer, D3FadingShape> expiringShapes;
-	private int shapesNum = 0;
+	private int spritesNum = 0;
 
 	private ShaderManager sm;
 	
 	ArrayList<Integer> toRemove = new ArrayList<Integer>();
 
-	private boolean removeShapeLater;
+	private boolean removeSpriteLater;
 	
 	public D3GLES20(ShaderManager shaderManager) {
-		shapes = new HashMap<Integer, D3Shape>();
+		sprites = new HashMap<Integer, D3Sprite>();
 		expiringShapes = new HashMap<Integer, D3FadingShape>();
-		shapesNum = 0;
+		spritesNum = 0;
 		sm = shaderManager;
-		removeShapeLater = false;
+		removeSpriteLater = false;
 	}
 	
 //	public static void init() {
@@ -42,24 +43,24 @@ public class D3GLES20 {
 //	}
 
 	public void draw(int key, float[] mMMatrix, float[] mVMatrix, float[] mProjMatrix) {
-		shapes.get(key).setModelMatrix(mMMatrix);
-		shapes.get(key).draw(mVMatrix, mProjMatrix);
+		sprites.get(key).getGraphic().setModelMatrix(mMMatrix);
+		sprites.get(key).getGraphic().draw(mVMatrix, mProjMatrix);
 	}
 	
 	public void drawAll(float[] mVMatrix, float[] mProjMatrix,
 			float interpolation) {
-		if (shapes == null) {
-			Log.w(TAG, "Shapes are null in drawAll!");
+		if (sprites == null) {
+			Log.w(TAG, "Sprites are null in drawAll!");
 			return;
 		}
 		toRemove.clear();
-		removeShapeLater = true;
-		for (D3Shape shape: shapes.values()) {
-			shape.draw(mVMatrix, mProjMatrix, interpolation);
+		removeSpriteLater = true;
+		for (D3Sprite sprite: sprites.values()) {
+			sprite.getGraphic().draw(mVMatrix, mProjMatrix, interpolation);
 		}
-		removeShapeLater = false;
+		removeSpriteLater = false;
 		for (Integer key: toRemove) {
-			shapes.remove(key);
+			sprites.remove(key);
 		}
 		
 		D3FadingShape shape;
@@ -74,16 +75,16 @@ public class D3GLES20 {
 		}
 	}
 	
-	public int putShape(D3Shape shape) {
-		if (shapes == null) {
+	public int putSprite(D3Sprite sprite) {
+		if (sprites == null) {
 			Log.w(TAG, "Shapes are null in putShape!");
 			return 0;
 		}
-		while (shapes.containsKey(shapesNum)) {
-			shapesNum++;
+		while (sprites.containsKey(spritesNum)) {
+			spritesNum++;
 		}
-		shapes.put(shapesNum, shape);
-		return shapesNum++;
+		sprites.put(spritesNum, sprite);
+		return spritesNum++;
 	}
 	
 	public void putExpiringShape(D3FadingShape shape) {
@@ -100,16 +101,14 @@ public class D3GLES20 {
 	}
 	
 	public void removeShape(int key) {
-		if (shapes == null) {
+		if (sprites == null) {
 			Log.w(TAG, "Shapes are null in removeShape!");
 			return;
 		}
-//		shapes.remove(key);
-		if (removeShapeLater) {
+		if (removeSpriteLater) {
 			toRemove.add(key);
-//			Log.v(TAG, "adding " + key + " to remove later");
 		}
-		else shapes.remove(key);
+		else sprites.remove(key);
 	}
 
 //	public void setShapePosition(int key, float x,
@@ -121,12 +120,12 @@ public class D3GLES20 {
 //		shapes.get(key).setPosition(x, y);
 //	}
 
-	public void clearGraphics() {
-		shapes.clear();
-		expiringShapes.clear();
-		shapes = null;
-		expiringShapes = null;
-	}
+//	public void clearGraphics() {
+//		shapes.clear();
+//		expiringShapes.clear();
+//		shapes = null;
+//		expiringShapes = null;
+//	}
 //	public void clean() {
 //		GLES20.glDeleteShader(defVertexShaderHandle);
 //		GLES20.glDeleteShader(defFragmentShaderHandle);
@@ -134,74 +133,75 @@ public class D3GLES20 {
 //		defFragmentShaderHandle = -1;
 //	}
 
-	public HashMap<Integer, D3Shape> getShapes() {
-		return shapes;
-	}
+//	public HashMap<Integer, D3Shape> getShapes() {
+//		return shapes;
+//	}
 
-	public void setShapes(HashMap<Integer, D3Shape> savedShapes) {
-		shapes.clear(); // maybe unnecessary
-		shapes.putAll(savedShapes);
-	}
+//	public void setShapes(HashMap<Integer, D3Shape> savedShapes) {
+//		shapes.clear(); // maybe unnecessary
+//		shapes.putAll(savedShapes);
+//	}
 	
-	public boolean contains(int key, float hX, float hY) {
-		if (shapes == null) {
+	public boolean contains(int key, PointF point) {
+		if (sprites == null) {
 			Log.w(TAG, "Shapes are null in contains!");
 			return false;
 		}
-		return D3Maths.circleContains(shapes.get(key).getCenterX(), shapes.get(key).getCenterY(), 
-				shapes.get(key).getRadius(), hX, hY);
+		return sprites.get(key).contains(point);//D3Maths.circleContains(shapes.get(key).getCenterX(), shapes.get(key).getCenterY(), 
+				//shapes.get(key).getRadius(), hX, hY);
 	}
 
-	public D3TempCircle newContainsCheckCircle(int key, float hX, float hY) {
-		return new D3TempCircle(shapes.get(key).getCenterX(), shapes.get(key).getCenterY(), 
-				shapes.get(key).getRadius(), TEMP_CIRCLE_TICKS, sm.getDefaultProgram());
-	}
-	
-	public int newDefaultQuad(float width, float height, float[] color) {
-		return putShape(new D3Quad(width, height, color, sm.getDefaultProgram()));
-	}
-
-	public int newDefaultCircle(float r, float[] color, int vertices) {
-		return putShape(new D3Circle(r, color, vertices, sm.getDefaultProgram()));
-	}
+//	public D3TempCircle newContainsCheckCircle(int key, float hX, float hY) {
+//		return new D3TempCircle(shapes.get(key).getCenterX(), shapes.get(key).getCenterY(), 
+//				shapes.get(key).getRadius(), TEMP_CIRCLE_TICKS, sm.getDefaultProgram());
+//	}
+//	
+//	public int newDefaultQuad(float width, float height, float[] color) {
+//		return putShape(new D3Quad(width, height, color, sm.getDefaultProgram()));
+//	}
+//
+//	public int newDefaultCircle(float r, float[] color, int vertices) {
+//		return putShape(new D3Circle(r, color, vertices, sm.getDefaultProgram()));
+//	}
 
 	public ShaderManager getShaderManager() {
 		return sm;
 	}
 
 	
-	public void setShapePosition(int key, float x,
-			float y) {
-		if (shapes == null) {
-			Log.w(TAG, "Shapes are null in setShapePosition!");
+	public void setSpritePosition(int key, PointF position) {
+		if (sprites == null) {
+			Log.w(TAG, "Sprites are null in setShapePosition!");
 			return;
 		}
-		if (!shapes.containsKey(key)) {
-			Log.w(TAG, "Shapes does not contain key " + key);
+		if (!sprites.containsKey(key)) {
+			Log.w(TAG, "Sprites does not contain key " + key);
 			return;
 		}
-		shapes.get(key).setPosition(x, y);
+		sprites.get(key).setPosition(position);
 	}
 	
-	public void setShapeVelocity(int key, float vx, float vy) {
-		if (shapes == null) {
-			Log.w(TAG, "Shapes are null in setShapeVelocity!");
+	public void setSpriteVelocity(int key, float vx, float vy) {
+		if (sprites == null) {
+			Log.w(TAG, "Sprites are null in setShapeVelocity!");
 			return;
 		}
-		shapes.get(key).setVelocity(vx, vy);
+		sprites.get(key).setVelocity(vx, vy);
 	}
 
-	public D3Shape getShape(int key) {
-		if (shapes == null) {
+	public D3Sprite getSprite(int key) {
+		if (sprites == null) {
 			Log.w(TAG, "Shapes are null in getShape!");
 			return null;
 		}
-		return shapes.get(key);
+		return sprites.get(key);
 	}
 
-	public boolean shapesCollide(D3Shape shape1, D3Shape shape2) {
+	public boolean spritesCollide(D3Sprite sprite1, D3Sprite sprite2) {
 		return D3Maths.circlesIntersect(
-				shape1.getCenterX(), shape1.getCenterY(), shape1.getRadius(), 
-				shape2.getCenterX(), shape2.getCenterY(), shape2.getRadius());
+//				sprite1.getCenterX(), sprite1.getCenterY(), sprite1.getRadius(), 
+//				sprite2.getCenterX(), sprite2.getCenterY(), sprite2.getRadius());
+				sprite1.getX(), sprite1.getY(), sprite1.getRadius(), 
+				sprite2.getX(), sprite2.getY(), sprite2.getRadius());
 	}
 }
---
+++ TheHunt/src/d3kod/d3gles20/D3Sprite.java
@@ -0,0 +1,99 @@
+package d3kod.d3gles20;
+
+import android.graphics.PointF;
+import android.util.Log;
+import d3kod.d3gles20.shapes.D3Shape;
+
+public abstract class D3Sprite {
+	private static final String TAG = "D3Sprite";
+	private D3Shape mGraphic;
+	private PointF mPos;
+	private PointF dirVector;
+	private boolean mGraphicSet;
+	
+	public D3Sprite(PointF position) {
+		mPos = new PointF(position.x, position.y);
+		dirVector = new PointF();
+		mGraphicSet = false; 
+//		mRemove = false;
+	}
+	
+	public void setPosition(PointF position) {
+		mPos.set(position);
+	}	
+	
+	public void update() {
+//		mX += vX; mY += vY;
+//		mPos.x += vX; mPos.y += vY;
+		mPos.x += dirVector.x;
+		mPos.y += dirVector.y;
+		mGraphic.setPosition(mPos.x, mPos.y);
+	}
+	
+	protected void initGraphic(D3Shape graphic) {
+		mGraphicSet = true;
+		mGraphic = graphic;
+//		mD3GLES20 = d3GLES20;
+//		mKey = mD3GLES20.putShape(mGraphic);
+//		mD3GLES20.setShapePosition(mKey, mX, mY);
+//		mD3GLES20.setShapeVelocity(mKey, vX, vY);
+		mGraphic.setPosition(mPos.x, mPos.y);
+		Log.v(TAG, "Set graphic for d3sprite at " + mPos.x + " " + mPos.y);
+	}
+	
+	abstract public void initGraphic();
+
+	public float getX() {
+		return mPos.x;
+	}
+	public float getY() {
+		return mPos.y;
+	}
+	
+	public void clearGraphic() {
+		if (!mGraphicSet) Log.v(TAG, "Graphic not set, can't clear!");
+//		else mD3GLES20.removeShape(mKey);
+		mGraphic = null;
+//		mRemove = true;
+	}
+	
+	public void setVelocity(float vx, float vy) {
+//		vX = vx; vY = vy;
+		dirVector.x = vx; dirVector.y = vy;
+		if (mGraphicSet) {
+//			mD3GLES20.setShapeVelocity(mKey, vx, vy);
+			mGraphic.setVelocity(vx, vy);
+		}
+	}
+	
+	public float getRadius() {
+//		if (!mGraphicSet) return 0; //TODO: dirty fix
+		return mGraphic.getRadius();
+	}
+	
+	public float getVX() {
+		return dirVector.x;
+	}
+	
+	public float getVY() {
+		return dirVector.y;
+	}
+
+	public boolean contains(PointF point) {
+		if (!mGraphicSet) {
+			Log.v(TAG, "Illegal contains call " + point.x +  " " + point.y + " because graphics are not set!");
+			return false;
+		}
+//		Log.v(TAG, "Contains check with center " + mX + " " + mY + " or should it be " + mGraphic.getCenterX() + " " + mGraphic.getCenterY());
+		return D3Maths.circleContains(mPos.x, mPos.y, getRadius(), point.x, point.y); //TODO: fix getRadius() call, keep seperate radiuses for graphic and logic
+	}
+	public D3Shape getGraphic() {
+		return mGraphic;
+	}
+	@Override
+	public String toString() {
+		return "D3Sprite " +  mPos;
+	}
+
+
+}
